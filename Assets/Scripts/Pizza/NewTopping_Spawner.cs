14b9b540e21aac8efe4641425bd0d9503d6ec426
Robert Grzechowiak <rgrzecho@fsc.int>
2018-03-21 11:50:50 -0400

    Orginization of Scripts added Pizza and Utility Folders, created Resources folder which allows loading of GameObjects into the game from within folder, created ResourceLoader script, started Refactor on NewTopping_Spawner

---
+++ Assets/Scripts/Pizza/NewTopping_Spawner.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace PizzaTime
+{
+    public class NewTopping_Spawner : MonoBehaviour
+    {
+
+        private GameObject spawn;
+        private GameObject sspawn;
+        private GameObject cspawn;
+        private GameObject rspawn;
+        private GameObject bspawn;
+        private GameObject pspawn;
+        private GameObject mspawn;
+        private GameObject pizzaObjForSpawner;
+        private GameObject sauceObjForSpawner;
+        private GameObject cheeseObjForSpawner;
+        private GameObject roniObjForSpawner;
+        private GameObject baconObjForSpawner;
+        private GameObject pepperObjForSpawner;
+        private GameObject mushObjForSpawner;
+        private ResourceLoader resourceLoader = new ResourceLoader();
+        public bool reset = false;
+
+        public Transform position;
+        Transform oldposition;
+
+        void Start()
+        {
+            oldposition = position;
+            // ------------------- Object Initalization ------------------------ \\
+            
+            pizzaObjForSpawner = resourceLoader.pizza;
+            sauceObjForSpawner = resourceLoader.sauce;
+            cheeseObjForSpawner = resourceLoader.cheese;
+            roniObjForSpawner = resourceLoader.roni;
+            baconObjForSpawner = resourceLoader.bacon;
+            pepperObjForSpawner = resourceLoader.pepper;
+
+            // ------------------------------------------------------------------ \\
+        }
+
+        void Update()
+        {
+        }
+
+        void OnCollisionEnter(Collision col)
+        {
+            GameObject coll = col.gameObject;
+            if (coll.tag == "dbutton")
+            {
+                Instantiate(pizzaObjForSpawner, spawn.transform.position, spawn.transform.rotation);
+            }
+            if (coll.tag == "tbutton")
+            {
+                GameObject[] s = GameObject.FindGameObjectsWithTag("sauce");
+                for (int i = 0; i < s.Length; i++) Destroy(s[i].gameObject);
+                GameObject[] c = GameObject.FindGameObjectsWithTag("cheese");
+                for (int i = 0; i < c.Length; i++) Destroy(c[i].gameObject);
+                GameObject[] r = GameObject.FindGameObjectsWithTag("roni");
+                for (int i = 0; i < r.Length; i++) Destroy(r[i].gameObject);
+                GameObject[] b = GameObject.FindGameObjectsWithTag("bacon");
+                for (int i = 0; i < b.Length; i++) Destroy(b[i].gameObject);
+                GameObject[] p = GameObject.FindGameObjectsWithTag("peppers");
+                for (int i = 0; i < p.Length; i++) Destroy(p[i].gameObject);
+                GameObject[] m = GameObject.FindGameObjectsWithTag("mushrooms");
+                for (int i = 0; i < m.Length; i++) Destroy(m[i].gameObject);
+                for (int i = 0; i < 2; i++) Instantiate(sauceObjForSpawner, sspawn.transform.position, sspawn.transform.rotation);
+                for (int i = 0; i < 2; i++) Instantiate(cheeseObjForSpawner, cspawn.transform.position, cspawn.transform.rotation);
+                for (int i = 0; i < 4; i++) Instantiate(baconObjForSpawner, bspawn.transform.position, bspawn.transform.rotation);
+                for (int i = 0; i < 4; i++) Instantiate(roniObjForSpawner, rspawn.transform.position, rspawn.transform.rotation);
+                for (int i = 0; i < 4; i++) Instantiate(pepperObjForSpawner, pspawn.transform.position, pspawn.transform.rotation);
+                for (int i = 0; i < 2; i++) Instantiate(mushObjForSpawner, mspawn.transform.position, mspawn.transform.rotation);
+            }
+            if (coll.tag == "button")
+            {
+                SceneManager.LoadScene("Level_GreyBox");
+            }
+            if (coll.tag == "reset")
+            {
+                SceneManager.LoadScene("Level_GreyBox");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/ResourceLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PizzaTime
+{
+    /// <summary>
+    /// Used for Loading Game Objects and/or Materials
+    /// </summary>
+    public class ResourceLoader : MonoBehaviour
+    {
+        public GameObject pizza;
+        public GameObject sauce;
+        public GameObject cheese;
+        public GameObject roni;
+        public GameObject bacon;
+        public GameObject pepper;
+        public GameObject mush;
+
+        // Loads Game Objects / Materials
+        void Start()
+        {
+            pizza = Resources.Load("Pie2.0") as GameObject;
+            sauce = Resources.Load("Sauce2.0") as GameObject;
+            cheese = Resources.Load("Cheese2.0") as GameObject;
+            roni = Resources.Load("Pepperoni2.0") as GameObject;
+            bacon = Resources.Load("Bacon2.0") as GameObject;
+            pepper = Resources.Load("Pepper2.0") as GameObject;
+            mush = Resources.Load("Mushroom2.0") as GameObject;
+        }
+    }
+}
