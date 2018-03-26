82db4e1458ed3420f99695aae42ebcb0a7edbc47
Robert Grzechowiak <rgrzecho@fsc.int>
2018-03-26 12:25:25 -0400

    Added Oven, OvenWood scripts to control how the tempature mechanic will work as well as testing prefabs

---
+++ Assets/Scripts/Oven/Oven.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PizzaTime
+{
+    public class Oven : MonoBehaviour
+    {
+        public float tempature;
+        public float maxTempature;
+
+        private GameObject pizzaOven;
+        private ResourceLoader resourceLoader;
+
+        void Start()
+        {
+            pizzaOven = this.gameObject;
+            resourceLoader = GameObject.FindObjectOfType<ResourceLoader>();
+        }
+
+        void TempatureDecreaseOverTime()
+        {
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/Oven/OvenWood.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PizzaTime
+{
+    public class OvenWood : MonoBehaviour
+    {
+        public float woodAddTemp;
+        private OvenWood ovenWood;
+        private ResourceLoader resourceLoader;
+
+        void Start()
+        {
+            ovenWood = this;
+            resourceLoader = GameObject.FindObjectOfType<ResourceLoader>();
+        }
+
+
+        //Adds Tempature to the Oven When Wood is added
+        void OnCollisionEnter(Collision col)
+        {
+            GameObject oven = col.gameObject;
+            if(oven.tag.Equals(resourceLoader.ovenObj.tag))
+            {
+                oven.GetComponent<Oven>().tempature += woodAddTemp;
+                Destroy(this.gameObject);
+            }
+        }
+
+    }
+}
+
