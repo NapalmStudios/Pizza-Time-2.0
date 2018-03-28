58511c7c3fac17d9b7f035a7f8ce9468fc4f2694
Jonathan Nneji <dankoswaggax@gmail.com>
2018-03-28 17:01:05 -0400

    worked on fixing the fixedjoint physics

---
+++ Assets/Scripts/VR-Fixing/VRTK_JointFix.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.GrabAttachMechanics;
+
+public class VRTK_JointFix : VRTK_FixedJointGrabAttach {
+
+    protected override void CreateJoint(GameObject obj)
+    {
+        base.CreateJoint(obj);
+
+    }
+}
