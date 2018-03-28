5126059cee377bbae0b4f44631d28c120541d005
Robert Grzechowiak <rgrzecho@fsc.int>
2018-03-28 12:02:06 -0400

    Started implmentation of squeeze bottle for sauce

---
+++ Assets/Scripts/Sauce/Sauce.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PizzaTime
+{
+    public class Sauce : MonoBehaviour
+    {
+        private GameObject sauceBottle;
+        private Vector3 bottleFoward;
+        // Use this for initialization
+        void Start()
+        {
+            sauceBottle = this.gameObject;
+            bottleFoward = sauceBottle.transform.forward;
+        }
+
+        //TODO: Add Raycast for Sauce Bottle
+    }
+}
+
