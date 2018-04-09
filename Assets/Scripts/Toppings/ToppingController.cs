e8854ef9159094f7e7a82b2a6461615814a0299a
Robert Grzechowiak <rgrzecho@fsc.int>
2018-04-09 14:15:28 -0400

    Toppings Refactor with New Controller Script

---
+++ Assets/Scripts/Toppings/ToppingController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PizzaTime
+{
+    public enum TOPPING
+    {
+        Sauce,
+        Cheese,
+        Roni,
+        Bacon,
+        Peppers,
+        Mushrooms
+    };
+
+    public class ToppingController : MonoBehaviour
+    {
+        public TOPPING toppingType;
+        public bool dirtyTopping = false;
+        public int timeToDirty;
+        private ResourceLoader resourceLoader;
+
+        void Start()
+        {
+            resourceLoader = GameObject.FindObjectOfType<ResourceLoader>();
+        }
+
+        private void OnCollisionStay(Collision col)
+        {
+            var objTag = col.gameObject.tag;
+            if(objTag.Equals(resourceLoader.floorObj.tag) && !gameObject.tag.Equals(resourceLoader.sauceObj.tag))
+            {
+                StartCoroutine(DirtyTopping());
+            }
+        }
+
+        private IEnumerator DirtyTopping()
+        {
+            yield return new WaitForSeconds(timeToDirty);
+            dirtyTopping = true;
+        }
+    }
+}
