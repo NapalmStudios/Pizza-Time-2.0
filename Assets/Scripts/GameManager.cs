b50a4190bdc99ca07e1b66ece0427616312f3077
Robert Grzechowiak <rgrzecho@fsc.int>
2018-04-25 10:18:05 -0400

    Revert to 26ef7c7

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public int cookTimer;
+    public int burnTimmer;
+    public List<Topping> multipleToppings = new List<Topping>();
+
+    public List<Ticket> currentTickets = new List<Ticket>();
+
+    public void AddToTicketList()
+    {
+        // Add new ticket to list od tickets
+        //currentTickets.Add()
+    }
+
+    private void RemoveFromList()
+    {
+        // remove forom list when pizza complete
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }        
+    }
+
+    public void PizzaCheck()
+    {
+        // check if pizza is correct to  a tick vurrent on a list
+    }
+}
---
+++ Assets/Scripts/Ticket.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ticket : MonoBehaviour
+{
+    public ToppingController ticketInfo;
+}
