29b1941155a4e0eafcf778f7edac92fa3ab54c54
Robert Grzechowiak <rgrzecho@fsc.int>
2018-04-26 16:19:14 -0400

    Pause Menu

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PizzaTime
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        public GameObject menu;
+
+        public void Pause()
+        {
+            if(menu.activeSelf)
+            {
+                //resume
+                menu.SetActive(false);
+            }
+            else
+            {
+                //pause
+                menu.SetActive(true);
+            }
+        }
+    }
+}
