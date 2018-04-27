4cf6a5dd32ff2593d575e1ae13266141a86e0eca
Robert Grzechowiak <rgrzecho@fsc.int>
2018-04-27 13:19:54 -0400

    Updated Pause Menu Script

--- Assets/Scripts/Menu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -4,36 +4,38 @@ using UnityEngine;
 
 namespace PizzaTime
 {
+    /*
+     * TODO: Fix Issue of Object coninuing to move when it shouldn't
+     *       Change Texture of Object
+     */
+
     public class PauseMenu : MonoBehaviour
     {
         public GameObject exitObj;
         public GameObject exitPizza;
         private GameObject follower;
 
+        private Quaternion exitRot;
+        private Quaternion exitPizRot;
+
         void Start()
         {
             follower = GameObject.Find("HeadsetFollower");
+            exitRot = exitObj.transform.rotation;
+            exitPizRot = exitPizza.transform.rotation;
         }
 
         public void Pause()
         {
-            /*if(menu.activeSelf)
-            {
-                //resume
-                menu.SetActive(false);
-            }
-            else
-            {
-                //pause
-                menu.SetActive(true);
-            } */
             var pos = follower.transform.position;
             if(!exitObj.activeSelf)
             {
                 exitObj.SetActive(true);
                 exitPizza.SetActive(true);
-                exitObj.transform.position = new Vector3(pos.x+.4f, pos.y, pos.z-.2f);
-                exitPizza.transform.position = new Vector3(pos.x+.4f, pos.y, pos.z+.2f);
+                exitObj.transform.position = new Vector3(pos.x+.4f, pos.y-.13f, pos.z-.2f);
+                exitObj.transform.rotation = new Quaternion(exitRot.x, exitRot.y, exitRot.z, exitRot.w);
+                exitPizza.transform.position = new Vector3(pos.x+.4f, pos.y-.13f, pos.z+.2f);
+                exitPizza.transform.rotation = new Quaternion(exitPizRot.x, exitPizRot.y, exitPizRot.z, exitPizRot.w);
             }
             else
             {
