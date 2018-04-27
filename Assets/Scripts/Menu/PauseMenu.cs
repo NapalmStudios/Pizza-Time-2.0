f674a60ea56a5456b4d7b83d4b621182b451aba6
Robert Grzechowiak <rgrzecho@fsc.int>
2018-04-27 12:00:59 -0400

    Updated Pause Menu Functionality and Design

--- Assets/Scripts/Menu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -6,11 +6,18 @@ namespace PizzaTime
 {
     public class PauseMenu : MonoBehaviour
     {
-        public GameObject menu;
+        public GameObject exitObj;
+        public GameObject exitPizza;
+        private GameObject follower;
+
+        void Start()
+        {
+            follower = GameObject.Find("HeadsetFollower");
+        }
 
         public void Pause()
         {
-            if(menu.activeSelf)
+            /*if(menu.activeSelf)
             {
                 //resume
                 menu.SetActive(false);
@@ -19,6 +26,19 @@ namespace PizzaTime
             {
                 //pause
                 menu.SetActive(true);
+            } */
+            var pos = follower.transform.position;
+            if(!exitObj.activeSelf)
+            {
+                exitObj.SetActive(true);
+                exitPizza.SetActive(true);
+                exitObj.transform.position = new Vector3(pos.x+.4f, pos.y, pos.z-.2f);
+                exitPizza.transform.position = new Vector3(pos.x+.4f, pos.y, pos.z+.2f);
+            }
+            else
+            {
+                exitObj.SetActive(false);
+                exitPizza.SetActive(false);
             }
         }
     }
