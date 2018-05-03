b81bbf46ae6e2680b54d4539a81bf757ee963e6e
Ethan Thibault <wickedlystudios@gmail.com>
2018-05-03 17:22:15 -0400

    Further Hierarchy, AudioLoader script, Will continue tomorrow

---
+++ Assets/Audio/AudioLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioLoader : MonoBehaviour {
+
+	void Awake () {
+		SceneManager.LoadScene("Audio", LoadSceneMode.Additive);
+		// Fabric.EventManager.Instance.PostEvent("Music - Start Main Music");
+	}
+}
